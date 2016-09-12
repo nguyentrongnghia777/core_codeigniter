8fac051101c8af88dc2b9bc6ec3610110719a100
nguyentrongnghia777 <nguyentrongnghia77@gmail.com>
2016-09-12 16:04:24 +0700

    clone CI 3.1.0

---
+++ application/controllers/Home.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+    /**
+     * Very first page of the web
+     */
+    public function index() {
+        echo 'hello word';
+    }
+}
