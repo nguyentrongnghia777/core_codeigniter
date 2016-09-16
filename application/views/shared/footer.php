217278537fb81721d580df592ab89e646b206fb7
nguyentrongnghia777 <nguyentrongnghia77@gmail.com>
2016-09-16 22:17:07 +0700

    add construct front-end

--- application/views/shared/footer.php
+++ application/views/shared/footer.php
@@ -8,5 +8,7 @@
         <!-- end footer -->
     </div>
     <!-- end container -->
+    <script type="text/javascript" src="<?=base_url('front-end/js/vendor/jquery-1.11.1.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('front-end/js/vendor/bootstrap.min.js');?>"></script>
 </body>
 </html>
