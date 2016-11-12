7822921ac4b90914e516c8fcfa54a81d5ce88f95
Nguyen Phu Dat <npdat132@gmail.com>
2016-11-12 21:41:35 +0700

    fix location file js and check spell word in file language

--- application/language/english/auth_lang.php
+++ application/language/english/auth_lang.php
@@ -129,7 +129,7 @@ $lang['edit_group_validation_desc_label']  = 'Description';
 // Change Password
 $lang['change_password_heading']                               = 'Change Password';
 $lang['change_password_old_password_label']                    = 'Old Password:';
-$lang['change_password_new_password_label']                    = 'New Password (at least 8 characters long):';
+$lang['change_password_new_password_label']                    = 'New Password (at least %s characters long):';
 $lang['change_password_new_password_confirm_label']            = 'Confirm New Password:';
 $lang['change_password_submit_btn']                            = 'Change';
 $lang['change_password_validation_old_password_label']         = 'Old Password';
@@ -148,7 +148,7 @@ $lang['forgot_password_email_not_found']         = 'No record of that email addr
 
 // Reset Password
 $lang['reset_password_heading']                               = 'Change Password';
-$lang['reset_password_new_password_label']                    = 'New Password (at least 8 characters long):';
+$lang['reset_password_new_password_label']                    = 'New Password (at least %s characters long):';
 $lang['reset_password_new_password_confirm_label']            = 'Confirm New Password:';
 $lang['reset_password_submit_btn']                            = 'Change';
 $lang['reset_password_validation_new_password_label']         = 'New Password';
